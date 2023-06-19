216df93ebdae76cecca268ebe93288a6360f9b60
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-06-19 23:31:42 +0100

    P1 - implemented consolidateItems method

--- src/main/java/com/interview/shoppingbasket/Basket.java
+++ src/main/java/com/interview/shoppingbasket/Basket.java
@@ -1,7 +1,9 @@
 package com.interview.shoppingbasket;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Basket {
     private List<BasketItem> items = new ArrayList<>();
@@ -19,7 +21,27 @@ public class Basket {
         return items;
     }
 
+    /**
+     * Consolidates the basket items by product code.
+     * The items are grouped in a map by their product code, and their quantities are merged.
+     * The items list is then replaced by the new consolidated items list.
+     */
     public void consolidateItems() {
-        // Exercise - implement this function
+        // Use a linked hash map here to maintain insertion order
+        Map<String, BasketItem> itemsByProductCode = new LinkedHashMap<>();
+        for (BasketItem item : items) {
+            itemsByProductCode.merge(item.getProductCode(), item, Basket::mergeQuantities);
+        }
+        this.items = new ArrayList<>(itemsByProductCode.values());
+        /* TODO
+         * - Can the product name be different for a same product code? If yes, what should be expected?
+         * - Can the list instance be replaced or should it be modified in-place? If not, the list should be final.
+         */
+    }
+
+    private static BasketItem mergeQuantities(BasketItem item1, BasketItem item2) {
+        int mergedQuantity = item1.getQuantity() + item2.getQuantity();
+        item1.setQuantity(mergedQuantity);
+        return item1;
     }
 }
