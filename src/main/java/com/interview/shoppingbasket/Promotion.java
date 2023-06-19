c364d7768460f3980d8ee90f12827d0bf696c81a
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-06-20 00:49:29 +0100

    P2 - promotion implementation

--- src/main/java/com/interview/shoppingbasket/Promotion.java
+++ src/main/java/com/interview/shoppingbasket/Promotion.java
@@ -1,5 +1,37 @@
 package com.interview.shoppingbasket;
 
+import lombok.Data;
+
+@Data
 public class Promotion {
-    // Not yet implemented
+
+    private final String productCode;
+    private final PromotionStrategy strategy;
+
+    public enum PromotionStrategy {
+
+        TEN_PERCENT_OFF {
+            @Override
+            public double apply(BasketItem item, double totalPrice) {
+                return totalPrice * 0.9;
+            }
+        },
+
+        FIFTY_PERCENT_OFF {
+            @Override
+            public double apply(BasketItem item, double totalPrice) {
+                return totalPrice * 0.5;
+            }
+        },
+
+        TWO_FOR_ONE {
+            @Override
+            public double apply(BasketItem item, double totalPrice) {
+                int halfQuantity = item.getQuantity() / 2;
+                return totalPrice - item.getProductRetailPrice() * halfQuantity;
+            }
+        };
+
+        public abstract double apply(BasketItem item, double totalPrice);
+    }
 }
