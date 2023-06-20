b6e94dcbe6517fa312dbc5f22047fdc24f2c5a09
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-06-20 23:08:27 +0100

    P2 - simplified promotion strategies tests

--- src/test/java/com/interview/shoppingbasket/PromotionTest.java
+++ src/test/java/com/interview/shoppingbasket/PromotionTest.java
@@ -4,27 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static com.interview.shoppingbasket.Promotion.PromotionStrategy.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PromotionTest {
 
     @Test
     void tenPercentOffTest() {
-        Promotion promotion = new Promotion("code", Promotion.PromotionStrategy.TEN_PERCENT_OFF);
-        BasketItem basketItem = new BasketItem();
-        basketItem.setQuantity(1);
-        basketItem.setProductRetailPrice(10.0);
-        double total = promotion.getStrategy().apply(basketItem, basketItem.getProductRetailPrice());
+        BasketItem basketItem = getBasketItem(1, 10.0);
+        double total = TEN_PERCENT_OFF.apply(basketItem, basketItem.getProductRetailPrice());
         assertEquals(9.0, total);
     }
 
     @Test
     void fiftyPercentOffTest() {
-        Promotion promotion = new Promotion("code", Promotion.PromotionStrategy.FIFTY_PERCENT_OFF);
-        BasketItem basketItem = new BasketItem();
-        basketItem.setQuantity(1);
-        basketItem.setProductRetailPrice(10.0);
-        double total = promotion.getStrategy().apply(basketItem, basketItem.getProductRetailPrice());
+        BasketItem basketItem = getBasketItem(1, 10.0);
+        double total = FIFTY_PERCENT_OFF.apply(basketItem, basketItem.getProductRetailPrice());
         assertEquals(5.0, total);
     }
 
@@ -37,11 +32,15 @@ class PromotionTest {
         "5, 5.0, 15.0",
     })
     void buyTwoPayOneTest(int quantity, double price, double expected) {
-        Promotion promotion = new Promotion("code", Promotion.PromotionStrategy.TWO_FOR_ONE);
+        BasketItem basketItem = getBasketItem(quantity, price);
+        double total = TWO_FOR_ONE.apply(basketItem, quantity * price);
+        assertEquals(expected, total);
+    }
+
+    private static BasketItem getBasketItem(int quantity, double price) {
         BasketItem basketItem = new BasketItem();
         basketItem.setQuantity(quantity);
         basketItem.setProductRetailPrice(price);
-        double total = promotion.getStrategy().apply(basketItem, quantity * price);
-        assertEquals(expected, total);
+        return basketItem;
     }
 }
